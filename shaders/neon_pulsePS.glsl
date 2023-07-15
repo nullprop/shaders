35b9c1f4f910d8a33a5d612677e61698e18b3050
nullprop <git@nullprop.sh>
2023-07-15 16:46:35 +0300

    Fix whitespace

--- shaders/neon_pulsePS.glsl
+++ shaders/neon_pulsePS.glsl
@@ -32,31 +32,31 @@ float rand(vec2 co){
 void main()
 {
     vec2 uv = (gl_FragCoord.xy * 2.0 - uResolution.xy) / uResolution.y;
-    
+
     // wobble
-	uv.x *= mix(1.0, sin(uTime * 0.5), 0.05);
-	uv.y *= mix(1.0, sin(uTime * 0.4), 0.05);
-    
+    uv.x *= mix(1.0, sin(uTime * 0.5), 0.05);
+    uv.y *= mix(1.0, sin(uTime * 0.4), 0.05);
+
     vec2 uv0 = uv;
     float dist = length(uv0);
     vec3 finalColor = vec3(0.0);
-    
+
     // foreground flash
     for (float i = 0.0; i < 2.0; i++)
     {
-    	uv = fract(uv * 0.4) - 0.5;
-    	uv *= cos(uTime * 0.5);
-
-    	vec3 col = palette(dist + sin(uTime * 0.1));
-
-		float d = length(uv);
-		d = sin(d * 12.0 + uTime * 0.5) / 12.0;
-		d = abs(d);
-		
-		d = 0.02 / d;
-		d = pow(d, 1.4);
-
-		finalColor += col * d;
+        uv = fract(uv * 0.4) - 0.5;
+        uv *= cos(uTime * 0.5);
+        
+        vec3 col = palette(dist + sin(uTime * 0.1));
+        
+        float d = length(uv);
+        d = sin(d * 12.0 + uTime * 0.5) / 12.0;
+        d = abs(d);
+        
+        d = 0.02 / d;
+        d = pow(d, 1.4);
+        
+        finalColor += col * d;
     }
 
     // noise
@@ -66,27 +66,27 @@ void main()
     finalColor.y = pow(finalColor.y, mix(1.0, noise, strength));
     finalColor.z = pow(finalColor.z, mix(1.0, noise, strength));
 
-	// background circles
+    // background circles
     uv = fract(uv0 * 30.5) - 0.5;
     uv *= sin(uTime * 0.5);
 
     vec3 col = palette2(dist + sin(uTime * 0.4));
+
+    float d = length(uv);
+    d = sin(d * 5.0 + uTime * 0.5) / 5.0;
+    d = abs(d);
     
-	float d = length(uv);
-	d = sin(d * 5.0 + uTime * 0.5) / 5.0;
-	d = abs(d);
-	
-	d = 0.01 / d;
-	d = pow(d, 0.5);
-	
-	finalColor += col * d;
-	
-	// vignette
-	vec2 edge = gl_FragCoord.xy / uResolution.xy;
-	edge *= 1.0 - edge.yx;
+    d = 0.01 / d;
+    d = pow(d, 0.5);
+
+    finalColor += col * d;
+
+    // vignette
+    vec2 edge = gl_FragCoord.xy / uResolution.xy;
+    edge *= 1.0 - edge.yx;
     float vig = edge.x * edge.y * 20.0;
     vig = pow(vig, 0.4);
     finalColor *= vig;
-    
+
     outColor = vec4(finalColor, 1.0);
 }
